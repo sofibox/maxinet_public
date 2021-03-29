1a7ed64995e7709d9c1675b01a5909aed33483b5
Arafat Ali <arafat@sofibox.com>
2021-03-29 19:08:34 +0800

    Added hash function for creating password
    Added argument to run preseed.cfg for both graphic and text setup
    Added timeout to skip setup menu for custom ISO

---
+++ git_lfs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Note for security purpose, use only in a single user system.
+# I use this in linux rescue mode to download git large file
+# For Debian & Ubuntu
+
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+SETUP_GIT_URL="https://token@github.com/sofibox/maxisetup.git"
+GIT_TOKEN="YOUR-GIT-TOKEN-KEY"
+
+# Install git
+if ! command -v git &>/dev/null; then
+	sudo apt-get -y install git
+fi
+# Install git-lfs, without this git will not download the large file into git directory
+# instead the file only contain the checksum and size details
+
+if ! command -v git-lfs &>/dev/null; then
+   curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+   sudo apt-get -y install git-lfs
+fi
+
+mkdir -p "${SCRIPT_PATH}/download"
+echo "Setting up temporary git authentication for private repository ..."
+cat /dev/null >"${SCRIPT_PATH}/download/.git-askpass"
+echo "#!/bin/bash" >>"${SCRIPT_PATH}/download/.git-askpass"
+echo "echo ${GIT_TOKEN}" >>"${SCRIPT_PATH}/download/.git-askpass"
+chmod +x "${SCRIPT_PATH}/download/.git-askpass"
+export GIT_ASKPASS="${SCRIPT_PATH}/download/.git-askpass"
+echo "Removing existing maxisetup repository ..."
+# remove existing clone
+rm -rf "${SCRIPT_PATH}/download/maxisetup"
+echo "Cloning new maxisetup repository ..."
+echo "=~=~=~=~=~=~=~"
+git clone "${SETUP_GIT_URL}"
+echo "=~=~=~=~=~=~=~"
+# Now we have setup folder.
+cp "${SCRIPT_PATH}/download/maxisetup/storage/os/debian/debian10-mod.iso" "${SCRIPT_PATH}/mini.iso"
+# Delete these files
+echo "Removing git authentication and destroying its environment variable ..."
+rm -f "${HOME}/.git-askpass"
+unset GIT_ASKPASS
